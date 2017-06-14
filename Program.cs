820dc93574e44df4e380b89b6b63c211df20ec2b
Mina Asham <mina.asham@hotmail.com>
2017-06-14 22:02:19 +0100

    Initialize project using "dotnet new console" and add VS solution file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PuzzleSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
