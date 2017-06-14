139711c3cb2c74d79f5623de12e88ac20155cba1
Mina Asham <mina.asham@hotmail.com>
2017-06-14 22:18:03 +0100

    Add PuzzleSolver class skeleton

---
+++ src/PuzzleSolver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PuzzleSolver
+{
+    class PuzzleSolver
+    {
+        public PuzzleSolver(int[] sumRows, int[] sumCols, int[,] initialPuzzle = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[,] Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
